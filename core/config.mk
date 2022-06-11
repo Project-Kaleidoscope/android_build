3f3148a3c0fcc2daae59cb84df51f673e1b64bd0
alk3pInjection <5e147612@kscope.ink>
2022-06-12 01:01:30 +0800

    build: Include new BoardConfig for Kaleidoscope builds
    Change-Id: Icc0fcadd47135b45e920a074c3d884d0fafbdafc

--- core/config.mk
+++ core/config.mk
@@ -317,7 +317,7 @@ FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 -include vendor/extra/BoardConfigExtra.mk
 ifneq ($(KSCOPE_BUILD),)
-include vendor/kscope/config/BoardConfigKScope.mk
+include vendor/kscope/target/board/BoardConfigKScope.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
